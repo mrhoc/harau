8355d103b63167515c91ca9d76668447f5eecbbd
tranvanhoc271 <tvh271@gmail.com>
2022-09-11 19:48:53 +0700

    commit

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -21,8 +21,9 @@ const Favorites=()=>{
             console.log(error);
           });
       }, [currentUser]);
-
-    return <>
+     
+ 
+    return <>   
         <div className="PageHeader__Container-sc-19x4r8h-1 fVgVuB">
                 <h2 className="PageHeader__Header-sc-19x4r8h-0 jTfioR">Sản phẩm yêu thích</h2>
             </div>
@@ -31,7 +32,7 @@ const Favorites=()=>{
                     favorites && favorites.map(product => <div key={product.key} className="ant-col ant-col-md-8 ant-col-xl-6" style={{ padding: 12 }}>
                     <article title={product.name} className="Card-gwofcb-0 MarketItem__Container-rr0c4s-0 gpLETI">
                         <div className="CardLink__Container-dqor2y-0 jRvebg">
-                            <div className="MarketItem__CardLinkContent-rr0c4s-3 hZWvVs">
+                            <div className="MarketItem__CardLinkContent-rr0c4s-3 hZWvVs" >
                                 <ProductsItem product={product}></ProductsItem>
                             </div>
                         </div>
